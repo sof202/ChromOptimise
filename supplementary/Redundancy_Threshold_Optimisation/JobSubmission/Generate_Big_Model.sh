ccaeba3e524e33b9e9c7c13d2a37510142776656
sof202 <s.o.fletcher@exeter.ac.uk>
2023-12-18 14:04:00 +0000

    clean up

--- supplementary/Redundancy_Threshold_Optimisation/JobSubmission/Generate_Big_Model.sh
+++ supplementary/Redundancy_Threshold_Optimisation/JobSubmission/Generate_Big_Model.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#SBATCH --export=ALL # export all environment variables to the batch job
-#SBATCH -p mrcq # submit to the mrc queue for faster queue times
-#SBATCH --time=40:00:00 # This can be a lengthy process, see BigModelLearningTimes.pdf for an idea of maximum wall time
+# Export all environment variables to the batch job
+#SBATCH --export=ALL 
+# Submit to the mrc queue for faster queue times
+#SBATCH -p mrcq 
+# This can be a lengthy process for particularly large models
+# A model with 80 states took 26 hours off of 1MB of binary data.
+# See BigModelLearningTimes.pdf for an idea of maximum wall time
+#SBATCH --time=40:00:00 
 #SBATCH -A Research_Project-MRC190311
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --mem=50G # specify bytes memory to reserve
+# Large memory consumption for big models, difficult to give good estimates
+#SBATCH --mem=32G 
 #SBATCH --mail-type=END # Send an email after the job is done
 # Temporary log file, later to be removed
 #SBATCH --output=temp%j.log
@@ -13,58 +19,57 @@
 #SBATCH --error=temp%j.err
 #SBATCH --job-name=Big_Model
 
-## -------------------------------------------------------------------------------------------- ##
-##                                                                                              ##
-##                                            PREAMBLE                                          ##
-##                                                                                              ##
-## -------------------------------------------------------------------------------------------- ##
-##                                            PURPOSE                                           ##
-## Generate a very large model using the binarized data generated in 4_BinarizeBamFiles.sh with ##
-##                    a random initialisation of parameters (with a set seed).                  ##
-## -------------------------------------------------------------------------------------------- ##
-##                        AUTHOR: Sam Fletcher s.o.fletcher@exeter.ac.uk                        ##
-##                                     CREATED: November 2023                                   ##
-## -------------------------------------------------------------------------------------------- ##
-##                                         PREREQUISITES                                        ##
-##                                    Run 4_BinarizeBamFiles.sh                                 ##
-## -------------------------------------------------------------------------------------------- ##
-##                                          DEPENDENCIES                                        ##
-##                                              Java                                            ##
-##                                            ChromHMM                                          ##
-## -------------------------------------------------------------------------------------------- ##
-##                                             INPUTS                                           ##
-##                                      $1 -> Size of model                                     ##
-##                                       $2 -> Random Seed                                      ##
-## -------------------------------------------------------------------------------------------- ##
-##                                            OUTPUTS                                           ##
-##        The emission parameter matrix of the model produced in .png, .txt and .svg form       ##
-##       The transition parameter matrix of the model produced in .png, .txt and .svg form      ##
-##               The full model file, including the two parameter matrices above                ##
-##                            The overlap and fold enrichment files                             ##
-##                            The log files produced by ChromHMM.jar                            ##
-##            The maximum achieved estimated log likelihood achieved by the model               ##
-## -------------------------------------------------------------------------------------------- ##
+## =================================================================================##
+##                                                                                  ||
+##                                     PREAMBLE                                     ||
+##                                                                                  ||
+## =================================================================================##
+## PURPOSE:                                                                         ||
+## Generate a very large model using the binarized data generated in                ||
+## 4_BinarizeBamFiles.sh with a random initialisation of parameters                 ||
+## (with a set seed).                                                               ||
+## =================================================================================##
+## AUTHOR: Sam Fletcher s.o.fletcher@exeter.ac.uk                                   ||
+## CREATED: November 2023                                                           ||
+## =================================================================================##
+## PREREQUISITES: Run 4_BinarizeBamFiles.sh                                         ||
+## =================================================================================##
+## DEPENDENCIES: Java, ChromHMM                                                     ||
+## =================================================================================##
+## INPUTS:                                                                          ||
+## $1 -> Size of model                                                              ||
+## $2 -> Random Seed                                                                ||
+## =================================================================================##
+## OUTPUTS:                                                                         ||
+## The emission parameter matrix of the model (.png,.txt,.svg)                      ||
+## The transition parameter matrix of the model (.png,.txt,.svg)                    ||
+## Full model file                                                                  ||
+## The overlap and fold enrichment files for existing genomic annotations           ||
+## The log files produced by ChromHMM.jar                                           ||
+## The maximum achieved estimated log likelihood achieved by the model              ||
+## =================================================================================##
 
-## ------------------------ ##
+## ======================== ##
 ##    HELP FUNCTIONALITY    ##
-## ------------------------ ##
+## ======================== ##
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    echo "===================================================================================================================="
-    echo "Purpose: Generates a model unrestricted by ChromHMM LearnModel's default size limits by using random initialisation."
+    echo "================================================================"
+    echo "Purpose: Generates a model unrestricted by ChromHMM LearnModel's"
+    echo "default size limits by using random initialisation."
     echo "Author: Sam Fletcher"
     echo "Contact: s.o.fletcher@exeter.ac.uk"
     echo "Dependencies: Java, ChromHMM"
     echo "Inputs:"
     echo "\$1 -> Size of model"
     echo "\$2 -> Random seed"
-    echo "===================================================================================================================="
+    echo "================================================================"
     exit 0
 fi
 
-## ------------ ##
+## ============ ##
 ##    SET UP    ##
-## ------------ ##
+## ============ ##
 
 echo "Job '$SLURM_JOB_NAME' started at:"
 date -u
@@ -90,15 +95,11 @@ ln "${SLURM_SUBMIT_DIR}/temp${SLURM_JOB_ID}.log" \
 ln "${SLURM_SUBMIT_DIR}/temp${SLURM_JOB_ID}.err" \
 "${LOG_FILE_PATH}/ModelSize-$1~${SLURM_JOB_ID}~$timestamp.err"
 
-
-# Variables set up
-MODEL_SIZE=$1
-SEED=$2
-
-# Check if binary files exist
-cd "${BINARY_DIR}" || { echo "Binary directory doesn't exist, make sure config.txt is pointing to the correct directory"; exit 1; }
+cd "${BINARY_DIR}" || { echo "Binary directory doesn't exist, \
+make sure config.txt is pointing to the correct directory"; exit 1; }
 if [ -z "$(ls -A)" ]; then
-    echo "4_BinarizedFiles is empty, ensure that 4_BinarizeBamFiles.sh has been ran before this script."
+    echo "4_BinarizedFiles is empty."
+    echo "Ensure that 4_BinarizeBamFiles.sh has been ran before this script."
     echo "Aborting..."
 
     # Remove temporary log files
@@ -108,7 +109,13 @@ if [ -z "$(ls -A)" ]; then
     exit 1
 fi
 
-# Check if inputs were given, if not set to defaults
+## ========================= ##
+##    VARIABLE ASSIGNMENT    ##
+## ========================= ##
+
+MODEL_SIZE=$1
+SEED=$2
+
 if [ -z "$MODEL_SIZE" ]; then
     echo "No model size was given by the user, using default value of 200."
     MODEL_SIZE=200
@@ -121,25 +128,36 @@ fi
 
 echo "Learning a model with ${MODEL_SIZE} states and with random seed: ${SEED}."
 
-## ---------- ##
+## ========== ##
 ##    MAIN    ##
-## ---------- ##
+## ========== ##
 
 module purge
 module load Java
 
-cd "${BIG_MODELS_DIR}" || { echo "Big models directory doesn't exist, make sure config.txt is pointing to the correct directory"; exit 1; }
+cd "${BIG_MODELS_DIR}" || { echo "Big models directory doesn't exist, \
+make sure config.txt is pointing to the correct directory"; exit 1; }
 
-java -mx30G -jar "${CHROMHMM_MAIN_DIR}/ChromHMM.jar" LearnModel -nobrowser -init random -s "${SEED}" "${BINARY_DIR}" "${BLUEPRINT_MAIN_DIR}/Big_Model_Files" "${MODEL_SIZE}" hg19 >"ChromHMM.Output.ModelSize.${MODEL_SIZE}.txt"
+java -mx30G \
+-jar "${CHROMHMM_MAIN_DIR}/ChromHMM.jar" LearnModel -nobrowser \
+-init random \
+-s "${SEED}" \
+"${BINARY_DIR}" "${BLUEPRINT_MAIN_DIR}/Big_Model_Files" "${MODEL_SIZE}" hg19 > \
+"ChromHMM.Output.ModelSize.${MODEL_SIZE}.txt"
 
 ## Find the estimated log likelihood from this file
 echo "Model Learning finished"
 echo "Writing estimated log likelihood to file"
-grep "       " "ChromHMM.Output.ModelSize.${MODEL_SIZE}.txt" | tail -1 | awk '{print $2}' >>"likelihood.ModelSize.${MODEL_SIZE}.txt" # grep removes the lines associated with writing to files. The tail and awk locate the final estimated log likelihood
+# grep removes the lines associated with writing to files. 
+# The tail and awk locate the final estimated log likelihood
+grep "       " "ChromHMM.Output.ModelSize.${MODEL_SIZE}.txt" | \
+tail -1 | \
+awk '{print $2}' >> \
+"likelihood.ModelSize.${MODEL_SIZE}.txt" 
 
-## ----------------------- ##
+## ======================= ##
 ##   LOG FILE MANAGEMENT   ##
-## ----------------------- ##
+## ======================= ##
 
 # Finishing message
 echo "Job completed at:"
