d21fe4de63d68338f13988b3ee7496805aee8f04
sof202 <s.o.fletcher@exeter.ac.uk>
2023-12-20 12:39:04 +0000

    remove unused scatter and histogram plots

--- supplementary/Redundancy_Threshold_Optimisation/Rscripts/HistogramPlotsForSimilarityMetrics_Transistions.r
+++ supplementary/Redundancy_Threshold_Optimisation/Rscripts/HistogramPlotsForSimilarityMetrics_Transistions.r
@@ -64,13 +64,6 @@ transition_state_to_maxima <- data.frame(State = integer(), Maxima = double())
 transition_state_from_maxima <- data.frame(State = integer(), Maxima = double())
 baseline_probability <- 1 / model_size
 
-# Find maximum transistion probability FROM each state
-for (state in 1:model_size){
-  maximum_transition_probability <- max(transition_data[state, ])
-  transition_state_from_maxima[nrow(transition_state_from_maxima) + 1, ] <-
-    c(state, maximum_transition_probability)
-}
-
 # Find maximum transistion probability TO each state
 for (state in 1:model_size){
   maximum_transition_probability <- max(transition_data[, state])
@@ -82,17 +75,6 @@ for (state in 1:model_size){
 ## ============ ##
 ##   PLOTTING   ##
 ## ============ ##
-
-transition_to_histogram <-
-  ggplot(transition_state_to_maxima, aes(x = Maxima)) +
-  geom_histogram(binwidth = baseline_probability / 2,
-                 color = "black", fill = "white") +
-  theme_minimal() +
-  geom_vline(xintercept = baseline_probability, color = "red") +
-  annotate("text", x = baseline_probability * 4, y = -1,
-           label = "Random chance", color = "red") +
-  labs(title = "Histogram for maximum probabilities of 
-       transitioning towards states")
 
 transition_to_scatter <-
   ggplot(transition_state_to_maxima, aes(x = State, y = Maxima)) +
@@ -109,78 +91,17 @@ transition_to_scatter <-
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
 
-transition_from_histogram <-
-  ggplot(transition_state_from_maxima, aes(x = Maxima)) +
-  geom_histogram(binwidth = baseline_probability / 2,
-                 color = "black", fill = "white") +
-  theme_minimal() +
-  geom_vline(xintercept = baseline_probability, color = "red") +
-  annotate("text", x = baseline_probability * 4, y = -1,
-           label = "Random chance", color = "red") +
-  labs(title = "Histogram for maximum probabilities 
-       of transitioning away from states")
-
-transition_from_scatter <-
-  ggplot(transition_state_from_maxima,
-         aes(x = State, y = Maxima)) +
-  geom_point() +
-  geom_hline(yintercept = baseline_probability, color = "red") +
-  scale_y_continuous(limits = c(0, NA),
-                     breaks = sort(c(seq(0, 1, by = 0.1),
-                                     baseline_probability))) +
-  scale_x_continuous(breaks = c(seq(0, model_size))) +
-  annotate("text", x = model_size - 2, y = baseline_probability / 2,
-           label = "Random chance", color = "red") +
-  labs(x = "Departing state of the model",
-       title = "Maximum probabilities of transitioning away from states") +
-  theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5))
-
 ## ================ ##
 ##   SAVING PLOTS   ##
 ## ================ ##
 
-transition_to_histogram_name <-
-  paste0("Histogram.Maximum.Probability.Transition.Away.model_size",
-         model_size, ".pdf")
-transition_from_histogram_name <-
-  paste0("Histogram.Maximum.Probability.Transition.Towards.model_size",
-         model_size, ".pdf")
-
 transition_to_scatter_name <-
   paste0("Scatter.Plot.Maximum.Probability.Transition.Towards.model_size",
          model_size, ".pdf")
-transition_from_scatter_name <-
-  paste0("Scatter.Plot.Maximum.Probability.Transition.Away.model_size",
-         model_size, ".pdf")
 
-ggsave(
-  transition_to_histogram_name,
-  plot = transition_to_histogram,
-  path = transition_plotting_dir,
-  height = 8.3,
-  width = 23.4,
-  units = "in"
-)
-ggsave(
-  transition_from_histogram_name,
-  plot = transition_from_histogram,
-  path = transition_plotting_dir,
-  height = 8.3,
-  width = 23.4,
-  units = "in"
-)
 ggsave(
   transition_to_scatter_name,
   plot = transition_to_scatter,
-  path = transition_plotting_dir,
-  height = 8.3,
-  width = 23.4,
-  units = "in"
-)
-ggsave(
-  transition_from_scatter_name,
-  plot = transition_from_scatter,
   path = transition_plotting_dir,
   height = 8.3,
   width = 23.4,
