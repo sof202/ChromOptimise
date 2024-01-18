001bdf99d985596543d0b4fd78f69caa86ffbc3c
sof202 <s.o.fletcher@exeter.ac.uk>
2024-01-18 11:19:50 +0000

    initial commit

---
+++ Rscripts/isolation_scores.R
@@ -0,0 +1,131 @@
+## ============================================================== ##
+##                                                                ||
+##                            PREAMBLE                            ||
+##                                                                ||
+## ============================================================== ##
+## PURPOSE:                                                       ||
+## Calculates the an estimated 'isolation score' for each state   ||
+## in the state assignment created by ChromHMM. 'Isolation score' ||
+## is given by 'the average number of bins separating two bins    ||
+## with the same state assignment'                                ||
+## ============================================================== ##
+## AUTHOR: Sam Fletcher                                           ||
+## CONTACT: s.o.fletcher@exeter.ac.uk                             ||
+## CREATED: January 2022                                          ||
+## ============================================================== ##
+## PREREQUISITES:                                                 ||
+## Run 5_batch_CreateIncrementalModels.sh                         ||
+## ============================================================== ##
+## INPUTS:                                                        ||
+## $1 -> Bin size                                                 ||
+## $2 -> Sample size                                              ||
+## $3 -> State assignments file                                   ||
+## $4 -> Sample size for isolation score                          ||
+## ============================================================== ##
+## OUTPUTS:                                                       ||
+## Plot of isolation score for each state                         ||
+## Text file containing isolation scores for each state           ||
+## ============================================================== ##
+
+## ========== ##
+##   SET UP   ##
+## ========== ##
+
+rm(list = ls())
+
+library("ggplot2")
+
+sample_size <- 100
+
+## =================== ##
+##   FILE PROCESSING   ##
+## =================== ##
+
+state_assignments <- read.table("test_file.txt", skip = 2)
+
+## ============================= ##
+##   ISOLATION SCORE FUNCTIONS   ##
+## ============================= ##
+
+check_upstream <- function(index, distance, reference_state) {
+  state <- state_assignments[index-distance, 1]
+  if (state == reference_state) {
+    return(distance - 1)
+  }
+  return(check_downstream(index, distance, reference_state))
+}
+
+check_downstream <- function(index, distance, reference_state) {
+  state <- state_assignments[index+distance, 1]
+  if (state == reference_state) {
+    return(distance - 1)
+  }
+  return(check_upstream(index, distance + 1, reference_state))
+}
+
+same_value_bin_distance <- function(index) {
+  reference_state <- state_assignments[index, 1]
+  distance <- check_upstream(index, 1, reference_state)
+  
+  return(distance)
+}
+
+get_isolation_score <- function(indices) {
+  sum_of_distances = 0
+  for (index in indices){
+    sum_of_distances = sum_of_distances + same_value_bin_distance(index)
+  }
+  sample_size <- length(indices)
+  average_distance <- (sum_of_distances / sample_size)
+  
+  return(average_distance)
+}
+
+## ========================= ##
+##   SUBSAMPLING FUNCTIONS   ##
+## ========================= ##
+
+subsample_bins_with_target_value <- function(target_state, sample_percent) {
+  indices_with_target_value <- which(state_assignments[,1] == target_state)
+  
+  sample_size <- length(indices_with_target_value) * (sample_percent / 100)
+  
+  sample_indices_with_target_value <- sample(indices_with_target_value, sample_size)
+  
+  return(sample_indices_with_target_value)
+}
+
+## ======== ##
+##   MAIN   ##
+## ======== ##
+
+states <- unlist(unique(state_assignments))
+samples <- rep(sample_size, times = length(states))
+
+samples_of_indices <- mapply(subsample_bins_with_target_value, states, samples)
+
+isolation_scores <- unlist(lapply(result, get_isolation_score))
+
+isolation_scores_output <- data.frame(states = states, isolation_scores = isolation_scores)
+
+sorted_isolation_scores <- isolation_scores_output[order(isolation_scores_output$states),]
+
+## =========== ##
+##   OUTPUTS   ##
+## =========== ##
+
+isolation_scores_scatter <- 
+  ggplot(sorted_isolation_scores, aes(x = states, y = isolation_scores)) +
+  geom_point() +
+  scale_x_continuous(breaks = seq(min(sorted_isolation_scores$states),
+                                  max(sorted_isolation_scores$states),
+                                  by = 1)) +
+  labs(title = "Isolation scores", x = "State number", y = "Isolation score") +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+write.table(sorted_isolation_scores, "isolation_scores.txt", row.names = FALSE)
+
+
+
