8a9f0386b2126fa4d05d769c01d403f6c1f38878
sof202 <s.o.fletcher@exeter.ac.uk>
2024-01-19 09:05:33 +0000

    clean up

--- Rscripts/IsolationScores.R
+++ Rscripts/IsolationScores.R
@@ -65,7 +65,7 @@ matching_bin_distance <- function(reference_bin_index) {
   # The maximum possible distance between two bins with the same state is
   # the number of bins between the reference bin index and the two ends of the
   # dataset
-  max_distance <- 
+  max_distance <-
     max(max_bin_index - reference_bin_index, reference_bin_index - 1)
 
   for (distance in 1:max_distance) {
@@ -113,7 +113,7 @@ get_isolation_score <- function(bin_indicies) {
   }
 
   average_distance <- (sum_of_distances / length(bin_indicies))
-  
+
   return(average_distance)
 }
 
