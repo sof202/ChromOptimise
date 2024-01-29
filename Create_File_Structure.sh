ce98a67e5f210f87e2141df05f680e573b2f37f8
sof202 <s.o.fletcher@exeter.ac.uk>
2024-01-29 12:41:25 +0000

    Add Create_File_Structure.sh script for automatic directory creation

---
+++ Create_File_Structure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## =================================================================================##
+##                                                                                  ||
+##                                     PREAMBLE                                     ||
+##                                                                                  ||
+## =================================================================================##
+## PURPOSE:                                                                         ||
+## Creates the file structure based off of the file structure that is given in      ||
+## the FilePaths.txt file in /configuration                                         ||
+## =================================================================================##
+## AUTHOR: Sam Fletcher                                                             ||
+## CONTACT: s.o.fletcher@exeter.ac.uk                                               ||
+## CREATED: January 2024                                                            ||
+## =================================================================================##
+
+source "/lustre/projects/Research_Project-MRC190311/scripts/integrative\
+/ChromOptimise/configuration/FilePaths.txt"
+
+# Create each data directory sequentially
+mkdir -p "${DOWNLOAD_DIR}"
+mkdir -p "${RAW_DIR}"
+mkdir -p "${PROCESSED_DIR}"
+mkdir -p "${SUBSAMPLED_DIR}"
+mkdir -p "${BINARY_DIR}"
+mkdir -p "${MODEL_DIR}"
+mkdir -p "${OPTIMUM_STATES_DIR}"
+mkdir -p "${COMPARE_DIR}"
+mkdir -p "${BIG_MODELS_DIR}"
