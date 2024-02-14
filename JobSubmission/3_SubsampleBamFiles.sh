43827152fd9685594dd5faa17cfea63693a1af61
sof202 <s.o.fletcher@exeter.ac.uk>
2024-02-14 11:25:33 +0000

    Make file 80 characters in width whever possible

--- JobSubmission/3_SubsampleBamFiles.sh
+++ JobSubmission/3_SubsampleBamFiles.sh
@@ -8,7 +8,8 @@
 #SBATCH -A Research_Project-MRC190311 
 #SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=16 
-# Predicted that memory consumption will rise massively when merging lots of files
+# Predicted that memory consumption will rise massively 
+# when merging lots of files (hundreds)
 #SBATCH --mem=10G 
 # Send an email after the job is done
 #SBATCH --mail-type=END 
@@ -18,35 +19,35 @@
 #SBATCH --error=temp%j.err
 #SBATCH --job-name=3_Merging_and_Subsampling
 
-## =================================================================================##
-##                                                                                  ||
-##                                     PREAMBLE                                     ||
-##                                                                                  ||
-## =================================================================================##
-## PURPOSE:                                                                         ||
-## Obtain a sample of the bam files. The bam files have varying sizes due to the    ||
-## number of reads. Sampling the files produced in 2_ProcessBamFiles.sh will lead   ||
-## to samples with the same number of files that contain a different number of      ||
-## reads. This removes the reproducability of the proceedure. To get around         ||
-## this, this script merges all of the processed .bam files and subsequently        ||
-## samples this larger file randomly. To save space, the merged file is deleted.    ||
-## =================================================================================##
-## AUTHOR: Sam Fletcher                                                             ||
-## CONTACT: s.o.fletcher@exeter.ac.uk                                               ||
-## CREATED: November 2023                                                           ||
-## =================================================================================##
-## PREREQUISITES: Run 2_batch_ProcessBamFiles.sh                                    ||
-## =================================================================================##
-## DEPENDENCIES: Samtools                                                           ||
-## =================================================================================##
-## INPUTS:                                                                          ||
-## -c|--config= -> Full (or relative) file path for configuation file directory     ||
-## -m|--mark= -> Epigenetic mark to process                                         ||
-## -s|--samplesize= -> Sample size as a percentage (default : 50)                   ||
-## =================================================================================##
-## OUTPUTS:                                                                         ||
-## Subsampled .bam files                                                            ||
-## =================================================================================##
+## ===========================================================================##
+##                                                                            ||
+##                                  PREAMBLE                                  ||
+##                                                                            ||
+## ===========================================================================##
+## PURPOSE:                                                                   ||
+## Obtain a sample of the bam files. The bam files have varying sizes due to  ||
+## the number of reads. Sampling the files produced in 2_ProcessBamFiles.sh   ||
+## will lead to samples with the same number of files that contain a different||
+## number of reads. This removes the reproducability of the proceedure.       ||
+## To get around this, this script merges all of the processed .bam files and ||
+## subsequently samples this larger file randomly.                            ||
+## ===========================================================================##
+## AUTHOR: Sam Fletcher                                                       ||
+## CONTACT: s.o.fletcher@exeter.ac.uk                                         ||
+## CREATED: November 2023                                                     ||
+## ===========================================================================##
+## PREREQUISITES: Run 2_batch_ProcessBamFiles.sh                              ||
+## ===========================================================================##
+## DEPENDENCIES: Samtools                                                     ||
+## ===========================================================================##
+## INPUTS:                                                                    ||
+## -c|--config= -> Full/relative file path for configuation file directory    ||
+## -m|--mark= -> Epigenetic mark to process                                   ||
+## -s|--samplesize= -> Sample size as a percentage (default : 50)             ||
+## ===========================================================================##
+## OUTPUTS:                                                                   ||
+## Subsampled .bam files                                                      ||
+## ===========================================================================##
 
 ## ===================== ##
 ##   ARGUMENT PARSING    ##
@@ -131,15 +132,16 @@ mv "${SLURM_SUBMIT_DIR}/temp${SLURM_JOB_ID}.err" \
 PROCESSED_FULL_FILE_PATH="${PROCESSED_DIR}/${mark_name}"
 
 if [[ -z "${mark_name}" ]]; then
-    { >&2 echo -e "ERROR: No epigenetic mark name given.\n\
-    Ensure that the first argument is the name of a processed epigenetic mark." ; \
+    { >&2 echo  "ERROR: No epigenetic mark name given. Ensure that the first" \
+    "argument is the name of a processed epigenetic mark."
     finishing_statement 1; }
 fi
 
-## ====== DEFAULTS ====================================================================
+## ====== DEFAULTS =============================================================
 if ! [[ "${sample_size}" =~ ^[0-9]+$ ]]; then
     sample_size=50
-    echo "Invalid sample size was given, using the default value of: ${sample_size}%."
+    echo "Invalid sample size was given, using the default value of:" \
+    "${sample_size}%."
 fi
 
 if [[ "${sample_size}" -gt 100 || "${sample_size}" -le 0 ]]; then
@@ -147,14 +149,15 @@ if [[ "${sample_size}" -gt 100 || "${sample_size}" -le 0 ]]; then
     sample_size=50
     echo "Using the default value of: ${sample_size}% instead."
 fi
-## ====================================================================================
+## =============================================================================
 
 ## ========================= ##
 ##   MERGING OF .BAM FILES   ##
 ## ========================= ##
 
 cd "${PROCESSED_FULL_FILE_PATH}" || \
-{ >&2 echo "ERROR: [\${PROCESSED_DIR}/\${mark_name} - ${PROCESSED_DIR}/${mark_name}] \
+{ >&2 echo "ERROR:" \
+"[\${PROCESSED_DIR}/\${mark_name} - ${PROCESSED_DIR}/${mark_name}] \
 doesn't exist, make sure that you typed the epigenetic mark correctly and that \
 FilePaths.txt is pointing to the correct directory."; finishing_statement 1; }
 
@@ -176,7 +179,8 @@ samtools merge -b List_Of_Bam_Files_To_Merge.txt "${output_file_path}"
 
 cd "${SUBSAMPLED_DIR}" || \
 { >&2 echo "ERROR: [\${SUBSAMPLED_DIR} - ${SUBSAMPLED_DIR} doesn't exist,] \
-make sure FilePaths.txt is pointing to the correct directory"; finishing_statement 1; }
+make sure FilePaths.txt is pointing to the correct directory"
+finishing_statement 1; }
 
 sample_size_decimal=$(echo "scale=2; $sample_size /100" | bc)
 echo "Subsampling merged .bam file with sample size ${sample_size}%..."
