bb3e78d6182c4eda3bb17a34073ee6c2d7ef8ef7
sof202 <s.o.fletcher@exeter.ac.uk>
2024-03-13 12:42:16 +0000

    fix input directory having spaces due to indentation

--- JobSubmission/7_RunLDSC.sh
+++ JobSubmission/7_RunLDSC.sh
@@ -177,8 +177,8 @@ if [[ -z "${bin_size}" || -z "${sample_size}" || -z "${number_of_models}" ]]; th
     sample_size=$(basename "${input_directory}" | cut -d_ -f4)
     number_of_models=$(basename "${input_directory}" | cut -d_ -f5)
 else
-    input_directory="${OPTIMUM_STATES_DIR} \
-    /BinSize_${bin_size}_SampleSize_${sample_size}_${number_of_models}"
+input_directory="${OPTIMUM_STATES_DIR} \
+/BinSize_${bin_size}_SampleSize_${sample_size}_${number_of_models}"
 fi
 
 if [[ -z "${model_size}" ]]; then
