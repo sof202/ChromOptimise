9a6be3428623ec523b55d22d651e7033eef8faea
sof202 <s.o.fletcher@exeter.ac.uk>
2024-08-22 13:12:59 +0100

    feat: add overlap threshold to config

--- Setup/Config.R
+++ Setup/Config.R
@@ -4,5 +4,9 @@ emissions_threshold <- VALUE
 isolation_threshold <- VALUE
 
 ## Number of marks used in analysis
+# Suggested values (for vanilla chromHMM, no changes to COORDS directory):
+# hg19 : 0.25
+# hg38 : 0.2
+overlap_threshold <- VALUE
 
 number_of_marks <- VALUE
