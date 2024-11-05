05d421756e74f652e2d2cf3531688d6df8507a85
sof202 <s.o.fletcher@exeter.ac.uk>
2024-11-05 15:01:23 +0000

    refactor: remove unnecessary library installation script

--- Rscripts/InstallLibraries.R
+++
@@ -1,5 +0,0 @@
-required_packages <- c("tidyr", "dplyr", "data.table", "ggplot2")
-
-installed_packages <- unlist(lapply(.libPaths(), list.files))
-
-install.packages(setdiff(required_packages, installed_packages))
